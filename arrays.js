ee3beefaa1d4c3f31c61b187d9abd6058df4fd57
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-18 12:47:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  var newArray = array.slic(1);
+  var newArray = array.slice(1);
   return newArray;
 }

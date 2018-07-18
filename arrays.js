64797bde499a454a3cdc26c1d8ebbe5c6f837022
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-18 12:24:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var makeNewArray = array.push(element);
+  var newArray = [element, ...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){

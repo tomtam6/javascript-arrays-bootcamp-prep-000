937fdbe3833039be56c726d70e519bd6bb7c1919
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-18 12:59:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
   array.slice(0, array.length - 1);
-  return newArray;
+  return array;
 }

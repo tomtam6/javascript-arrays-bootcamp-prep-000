f68894b21b2b46cc36b4bbea597b6ba47537dd86
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-18 12:38:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array;
 }
 
-function addelementToEndOfArray(array, element){
+function addElementToEndOfArray(array, element){
   var newArray = [...array, element];
   return newArray;
 }
